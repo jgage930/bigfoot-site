de18f90368bf78a4933b2648fc7c1b6237ab8c28
jgage930 <jgage930@gmail.com>
2022-10-12 09:09:58 -0500

    read routes boiler plate

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,24 @@ from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
+app = FastAPI()
+
+@app.get('/sightings')
+def get_sightings():
+	""" Returns all sightings as json"""
+	pass
+
+@app.get('/sightings/{id}')
+def get_sightings(id: int):
+	""" Returns sighting by id """
+	pass
+
+@app.get('/sightings/{state}/{county}')
+def get_sightings(state: str, county: str):
+	""" return a list of sightings for a county """
+	pass
+
+@app.get('/sightings/{state}')
+def get_sightings(state: str):
+	""" return a list of sightings ids by state """
+	pass
--- database/read.py
+++ database/read.py
@@ -1,3 +1,23 @@
-from database.setup import create_connection
+from database.setup import create_connection, DATABASE
 from database.models import Sighting
 
+
+def read_all_sightings():
+	with create_connection(DATABASE) as conn:
+		c = conn.cursor()
+
+		query = """
+			SELECT * FROM sightings;
+		"""
+
+		c.execute(query)
+
+		data = c.fetchall()
+
+	result = {"sightings": []}
+	for line in data:
+		id, *report = line
+
+		result["sightings"].append(Sighting(*report).to_json())
+
+	return result
