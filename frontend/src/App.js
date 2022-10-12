cad0255d332099c425f15a3394db8ce4bb9dafb7
jgage930 <jgage930@gmail.com>
2022-10-12 15:52:20 -0500

    sightings view working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,19 +9,29 @@ function Sighting () {
 
   const getData = async () => {
     let res = await axios.get('http://127.0.0.1:8000/sightings/3');
-    setResult(res.data);
+    
+    let data = res.data;
+    let final = data.sightings[0];
+    setResult(final)
   }
 
   useEffect(() =>{
     getData()
   }, [])
 
-  console.log(result);
-
+  
   return(
     <div>
       <h3>Sighting</h3>
       <p>sighting data</p>
+      {Object.keys(result).map((key, index) => {
+        return (
+          <div>
+            <h2>{key}</h2>
+            <p>{result[key]}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
