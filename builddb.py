8d7e218bb23c2e289fdaa932068ae507c2363124
jgage930 <jgage930@gmail.com>
2022-10-12 08:52:18 -0500

    builddb.py file added

--- builddb.py
+++ builddb.py
@@ -1,8 +1,5 @@
-from database.setup import create_tables
-from database.models import Sighting
 from database.functions import parse_reports
 from database.create import insert_sighting
-import json
 
 """Run this file to rebuild data base and insert data from the reports.json"""
 
