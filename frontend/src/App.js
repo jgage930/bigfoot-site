7aaf33fb52d10ab957019c8a76e34374e29fd8b8
jgage930 <jgage930@gmail.com>
2022-10-13 08:32:30 -0500

    working sighting view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import './App.css';
+import Sighting from './sighting';
 
-
-function Sighting () {
-
-  const [result, setResult] = useState({'hey': 'yes'});
-
-  const getData = async () => {
-    let res = await axios.get('http://127.0.0.1:8000/sightings/3');
-    
-    let data = res.data;
-    let final = data.sightings[0];
-    setResult(final)
-  }
-
-  useEffect(() =>{
-    getData()
-  }, [])
-
-  
-  return(
-    <div>
-      <h3>Sighting</h3>
-      <p>sighting data</p>
-      {Object.keys(result).map((key, index) => {
-        return (
-          <div>
-            <h2>{key}</h2>
-            <p>{result[key]}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
 
 function App() {
   return (
